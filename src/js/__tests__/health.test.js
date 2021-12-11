ebaa2a29a5fa023f1575e9d53c875ff36cb97fcb
Siun Maksim <myrdym@rambler.ru>
2021-12-11 22:04:09 +0300

    test add

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -8,3 +8,13 @@ test.each([
   const result = getHealth(info);
   expect(result).toBe(expected);
 });
+
+test.each([
+
+  [{ name: 'magician', health: 13 }, 'wounded'],
+  [{ name: 'magician', health: 48 }, 'critical'],
+  [{ name: 'magician', health: 92 }, 'critical'],
+])('%o', (info, expected) => {
+  const result = getHealth(info);
+  expect(result).not.toBe(expected);
+});
